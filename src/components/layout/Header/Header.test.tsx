53aecdb5495418ab4a1a42786a4bec77a11e96e7
Barvinko <vlad.barvinko@gmail.com>
2025-10-25 00:46:33 +0300

    feat: add API request for top films to header component

--- src/components/layout/Header/Header.test.tsx
+++ src/components/layout/Header/Header.test.tsx
@@ -1,21 +1,34 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { screen, fireEvent, waitFor } from '@testing-library/react';
 import { Header } from './Header';
+import { useRouter, useParams } from 'next/navigation';
 import { ThemeProvider } from '@components/providers/ThemeProvider/ThemeProvider';
+import { renderWithStore } from '@utilities/renderWithStore';
+
+jest.mock('next/navigation', () => ({
+  useRouter: jest.fn(),
+  useParams: jest.fn(),
+}));
 
 describe('Header Component', () => {
+  const mockPush = jest.fn();
+
   beforeEach(() => {
-    render(
+    jest.clearAllMocks();
+    (useRouter as jest.Mock).mockReturnValue({ push: mockPush });
+    (useParams as jest.Mock).mockReturnValue({ page: '1' });
+
+    renderWithStore(
       <ThemeProvider>
         <Header />
       </ThemeProvider>
     );
   });
 
-  test('renders Header component', () => {
+  it('renders Header component', () => {
     expect(screen.getByText('TMDB')).toBeInTheDocument();
   });
 
-  test('toggles theme when button is clicked', () => {
+  it('toggles theme when button is clicked', () => {
     const button = screen.getByRole('button');
     expect(screen.getByTestId('moon-icon')).toBeInTheDocument();
 
@@ -24,5 +37,14 @@ describe('Header Component', () => {
 
     fireEvent.click(button);
     expect(screen.getByTestId('moon-icon')).toBeInTheDocument();
+  });
+
+  it('click on header link and go to page', async () => {
+    fireEvent.click(screen.getByText('TMDB'));
+    expect(mockPush).toHaveBeenCalled();
+
+    await waitFor(() => {
+      expect(mockPush).toHaveBeenCalledWith('/page/1');
+    });
   });
 });
